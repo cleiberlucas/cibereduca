f3a00522fc886eea9853ab682ffeb8563f8dbd27
Cleiber <cleiberlucas@gmail.com>
2020-06-02 18:18:13 -0300

    update

--- routes/web.php
+++ routes/web.php
@@ -54,7 +54,7 @@ Route::prefix('secretaria')
 /**
  * Rotas Admin
  */
-Route::prefix('admin', [], false)
+Route::prefix('admin', 0)
         ->namespace('Admin')        
         ->middleware('auth')
         ->group(function(){                
