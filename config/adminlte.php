4eec10ed079302eaa5b11777d30b5921a9e17a85
Cleiber <cleiberlucas@gmail.com>
2020-06-03 19:43:13 -0300

    Login

--- config/adminlte.php
+++ config/adminlte.php
@@ -45,7 +45,7 @@ return [
     |
     */
 
-    'logo' => '<b>Rede </b>Educa',
+    'logo' => 'Rede Educa',
     'logo_img' => 'vendor/adminlte/dist/img/logo.png',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
