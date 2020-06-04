9b687c34ff782d50a659f0a05a1cbb0d0277d4de
Cleiber <cleiberlucas@gmail.com>
2020-06-03 23:07:09 -0300

    update

--- resources/views/principal/paginas/home/index.blade.php
+++ resources/views/principal/paginas/home/index.blade.php
@@ -11,10 +11,16 @@
 @section('content')
     <div class="card">
         <div class="card-header">
-            <h1>Colégio Rede Educa Goiás</h1>
+            <h2>Colégio Rede Educa Goiás</h2>
         </div>
         <div class="row card-body justify-content-center">         
             <img width="85%" height="85%" src="vendor/adminlte/dist/img/rede-banner4.png" alt="">            
+        </div>
+        <div class="row justify-content-center">            
+            
+                <img width="2%" src="vendor/cibersys/img/cubo_magico.gif" alt="">   
+                CiberSys - Sistemas Inteligentes
+            
         </div>
     </div>
 @stop
