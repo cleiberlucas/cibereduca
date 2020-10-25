5c1515485791d727494312d56778226cd4ffa78f
Cleiber <cleiberlucas@gmail.com>
2020-10-25 11:46:28 -0300

    Update boletim.blade.php

--- resources/views/pedagogico/paginas/turmas/relatorios/boletim.blade.php
+++ resources/views/pedagogico/paginas/turmas/relatorios/boletim.blade.php
@@ -1,11 +1,10 @@
-@section('title_postfix', ' Boletim {{$turma->nome_turma}}')
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-   {{--  <title>Boletim {{$turma->nome_turma}}</title> --}}
+    <title>Boletim {{$turma->nome_turma}}</title>
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
