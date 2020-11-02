0701735f8b766bec22598f370cec49297959794a
Cleiber <cleiberlucas@gmail.com>
2020-11-02 20:20:10 -0300

    Update ficha_matricula.blade.php

--- resources/views/secretaria/paginas/matriculas/ficha_matricula.blade.php
+++ resources/views/secretaria/paginas/matriculas/ficha_matricula.blade.php
@@ -18,130 +18,123 @@
         
 </head>
 <style>
-    html {
-        height: 100%;
-    }
-
-    body, .container {
-        height: 100%;
-    }
-
-    .row {    
+   .row {    
         margin-top: -0.25rem !important;
+        /* margin-right: 5; */
         margin-bottom: 0;
+        /* padding-right: 5; */
         padding-top: 0;
         padding-bottom: 0
     }
-
-   /*  table.report-container {
-    page-break-after:always;
+    p{
+        font-size: 20px;
     }
-    thead.report-header {
-        display:table-header-group;
+    html {
+        height: 96%;
     }
-    tfoot.report-footer {
-        display:table-footer-group;
-    } 
 
-    .container img {
-        max-width:200px;
-        max-height:150px;
-        width: auto;
-        height: auto;
-    } */
+    body {
+        min-height: 100%;
+        display: grid;
+        grid-template-rows: 1fr auto;
+    }
+
+    .footer {
+        grid-row-start: 2;
+        grid-row-end: 3;
+    }
 </style>
 
 <body>
-    <div class="container-fluid">        
-        @include('secretaria.paginas._partials.cabecalho')
-                       
-        {{-- cabeçalho da ficha --}}
-        @foreach ($matriculas as $index => $matricula)
-            @if ($index == 0)
-                <hr>
-                
-                <div class="row">
-                    <div class="form-group col-sm-10 col-xs-2 my-0 py-0" >                                                            
-                        <font size=4><center><strong> FICHA DE MATRÍCULA </strong></center></font>
-                        <br>
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="form-group col-sm-7 col-xs-2 my-0 py-0">
-                        <strong>Nome do aluno(a): {{$matricula->aluno->nome}}  </strong>
-                    </div>
-                    <div class="form-group col-sm-4 col-xs-2 my-0 py-0">
-                        Sexo: {{$matricula->aluno->sexo->sexo}}
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="form-group col-sm-4 col-xs-2 my-0 py-0">                    
-                        Data de Nascimento: {{date('d/m/Y', strtotime($matricula->aluno->data_nascimento))}} &nbsp&nbsp&nbsp 
-                    </div>
-                    <div class="form-group col-sm-4 col-xs-2 my-0 py-0">
-                        Cidade: &nbsp&nbsp&nbsp&nbsp UF:
-                    </div>
-                        {{-- CPF: {{mascaraCpfCnpj('###.###.###-##', $matricula->aluno->cpf)}}    &nbsp&nbsp&nbsp Fone: {{mascaraTelefone("(##) #####-####",$matricula->aluno->telefone_1)}} --}}
-                        
+    <div class="container-fluid">    
+        
+        @include('secretaria.paginas._partials.cabecalho_redeeduca')
+        <div class="content">               
+            {{-- cabeçalho da ficha --}}
+            @foreach ($matriculas as $index => $matricula)
+                @if ($index == 0)
+                    <hr>
                     
-                </div>
-                <div class="row">
-                    <div class="form-group col-sm-10 col-xs-2 my-0 py-0">
-                        Filiação: {{$matricula->aluno->pai}} 
+                    <div class="row">
+                        <div class="form-group col-sm-12 col-xs-2 my-0 py-0" >                                                            
+                            <font size=4><center><strong> FICHA DE MATRÍCULA </strong></center>
+                            <br>
+                        </div>
                     </div>
-                </div>
-                <div class="row">
-                    <div class="form-group col-sm-10 col-xs-2 my-0 py-0">
-                        e: {{$matricula->aluno->mae}}
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="form-group col-sm-10 col-xs-2 my-0 py-0">
-                        Endereço: {{$matricula->responsavel->endereco->endereco ?? ''}} - {{$matricula->responsavel->endereco->complemento ?? ''}} &nbsp&nbsp&nbsp Nº {{$matricula->responsavel->endereco->numero ?? ''}}
-                        <br>
-                        Bairro: {{$matricula->responsavel->endereco->bairro ?? ''}} &nbsp&nbsp&nbspCidade: {{$matricula->responsavel->endereco->cidade->cidade ?? ''}}/{{$matricula->responsavel->endereco->cidade->estado->sigla ?? ''}}    
-                        &nbsp&nbsp&nbsp CEP: 
-                        @if (isset($matricula->responsavel->endereco->cep)) 
-                            {{mascaraCEP('##.###-###', $matricula->responsavel->endereco->cep) ?? ''}}
-                        @endif
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="form-group col-sm-10 col-xs-2 my-0 py-0">
-                        Fones: 
-                        @if (strlen($matricula->responsavel->telefone_1) == 10)
-                            {{mascaraTelefone("(##) ####-####",$matricula->responsavel->telefone_1)}}
-                        @else
-                            {{mascaraTelefone("(##) #####-####",$matricula->responsavel->telefone_1)}}
-                        @endif
-                        e 
-                        @if (strlen($matricula->responsavel->telefone_2) == 10)
-                            {{mascaraTelefone("(##) ####-####",$matricula->responsavel->telefone_2)}}
-                        @else
-                            {{mascaraTelefone("(##) #####-####",$matricula->responsavel->telefone_2)}}
-                        @endif
-                        
-                        {{-- &nbsp&nbsp&nbsp E-mail: {{$matricula->responsavel->email_1}} --}}
-                    </div>
-                </div>
 
-                <div class="row mb-4" >
-                    <div class="form-group col-sm-10 col-xs-2 my-0 py-0">
-                        OBS:
+                    <div class="row">
+                        <div class="form-group col-sm-8 col-xs-2 my-0 py-0">
+                            <strong>Nome do aluno(a): {{$matricula->aluno->nome}}  </strong>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="form-group col-sm-6 col-xs-2 my-0 py-0">                    
+                            Data de Nascimento: {{date('d/m/Y', strtotime($matricula->aluno->data_nascimento))}} &nbsp&nbsp&nbsp Sexo: {{$matricula->aluno->sexo->sexo}}
+                        </div>
+                        {{-- <div class="form-group col-sm-4 col-xs-2 my-0 py-0">
+                            Cidade: &nbsp&nbsp&nbsp&nbsp UF:
+                        </div> --}}
+                            {{-- CPF: {{mascaraCpfCnpj('###.###.###-##', $matricula->aluno->cpf)}}    &nbsp&nbsp&nbsp Fone: {{mascaraTelefone("(##) #####-####",$matricula->aluno->telefone_1)}} --}}
+                            
                         
                     </div>
-                </div>
-            </div>
-            @endif
-            @break;
-        @endforeach {{-- fim cabeçalho da ficha --}}
+                    <div class="row">
+                        <div class="form-group col-sm-12 col-xs-2 my-0 py-0">
+                            Filiação: {{$matricula->aluno->pai}} 
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="form-group col-sm-12 col-xs-2 my-0 py-0">
+                            e: {{$matricula->aluno->mae}}
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="form-group col-sm-12 col-xs-2 my-0 py-0">
+                            Endereço: {{$matricula->responsavel->endereco->endereco ?? ''}} - {{$matricula->responsavel->endereco->complemento ?? ''}} &nbsp&nbsp&nbsp Nº {{$matricula->responsavel->endereco->numero ?? ''}}
+                            <br>
+                            Bairro: {{$matricula->responsavel->endereco->bairro ?? ''}} &nbsp&nbsp&nbspCidade: {{$matricula->responsavel->endereco->cidade->cidade ?? ''}}/{{$matricula->responsavel->endereco->cidade->estado->sigla ?? ''}}    
+                            &nbsp&nbsp&nbsp CEP: 
+                            @if (isset($matricula->responsavel->endereco->cep)) 
+                                {{mascaraCEP('##.###-###', $matricula->responsavel->endereco->cep) ?? ''}}
+                            @endif
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="form-group col-sm-12 col-xs-2 my-0 py-0">
+                            Fones: 
+                            @if (strlen($matricula->responsavel->telefone_1) == 10)
+                                {{mascaraTelefone("(##) ####-####",$matricula->responsavel->telefone_1)}}
+                            @else
+                                {{mascaraTelefone("(##) #####-####",$matricula->responsavel->telefone_1)}}
+                            @endif
+                            e 
+                            @if (strlen($matricula->responsavel->telefone_2) == 10)
+                                {{mascaraTelefone("(##) ####-####",$matricula->responsavel->telefone_2)}}
+                            @else
+                                {{mascaraTelefone("(##) #####-####",$matricula->responsavel->telefone_2)}}
+                            @endif
+                            
+                            {{-- &nbsp&nbsp&nbsp E-mail: {{$matricula->responsavel->email_1}} --}}
+                        </div>
+                    </div>
+
+                    <div class="row" >
+                        <div class="form-group col-sm-12 col-xs-2 my-0 py-0">
+                            OBS:                            
+                        </div>
+                    </div>
+               
+                @endif
+                @break;        
+
+            @endforeach {{-- fim cabeçalho da ficha --}}
+        </div>
                 
         <hr>
-        <font size="2">
+        <font size="3">
         @foreach ($matriculas as $index => $matricula)                    
                                 
-            <div class="row col-sm-5 ml-2 " style="outline: 1px solid black">
+            <div class="row col-sm-6 ml-2 " style="outline: 1px solid black">
                 
                 <div class="row" >
                     <div class="form-group col-sm-12 col-xs-2 mt-2 mb-0 py-0" >
@@ -151,10 +144,11 @@
                 </div>
                 <div class="row mx-auto">
                     <div class="form-group col-sm-12 col-xs-2 mt-4 mb-0 py-0" align="center">                                
+                        <br>
                         _________________________________________
                         <br>
                         Assinatura do Responsável pelo Aluno
-                        <br><br>
+                        <br><br><br>
                         _________________________________________
                         <br>
                         Assinatura do Diretor
@@ -168,6 +162,6 @@
                 {{-- &nbsp&nbsp&nbsp Série/Ano: {{$matricula->turma->nome_turma}} - {{$matricula->turma->tipoTurma->subNivelEnsino->sub_nivel_ensino ?? ''}} - {{$matricula->turma->tipoTurma->anoLetivo->ano ?? ''}} --}}
                    
     </div>
-    
+    @include('secretaria.paginas._partials.rodape_redeeduca')
 </body>
 </html>
