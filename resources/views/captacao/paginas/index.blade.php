4ba7cb3302d95dc1132fab057e99ee501e451d12
Cleiber <cleiberlucas@gmail.com>
2020-11-04 19:03:20 -0300

    Update index.blade.php

--- resources/views/captacao/paginas/index.blade.php
+++ resources/views/captacao/paginas/index.blade.php
@@ -25,49 +25,51 @@
             </form>
         </div>
         <div class="card-body">
-                <table class="table table-responsive table-condensed">
-                    <table class="table table-hover">
-                        <thead>
-                            <th>Ano Letivo</th>                        
-                            <th>Interessado</th>                        
-                            <th>Aluno</th>
-                            <th>Série</th>
-                            <th>Situação</th>                        
-                            <th width="170">Ações</th>
-                        </thead>
-                        <tbody>   
-                            @foreach ($captacoes as $captacao)                       
-                                <tr>
-                                    <td>
-                                        {{$captacao->ano}}
-                                    </td> 
-                                    <td>
-                                        {{$captacao->nome}}
-                                    </td>                   
-                                    <td>
-                                        {{$captacao->aluno}}                                      
-                                    </td>              
-                                    <td>{{$captacao->serie_pretendida}}</td>
-                                    <td>{{$captacao->tipo_negociacao}}</td>
-                                    <td style="width=10px;">
-                                        <a href="{{ route('historico.create', $captacao->id_captacao) }}" class="btn btn-sm btn-outline-primary"><i class="fas fa-paste"></i></a>
-                                        <a href="{{ route('captacao.edit', $captacao->id_captacao) }}" class="btn btn-sm btn-outline-success"><i class="fas fa-edit"></i></a>
-                                        <a href="{{ route('captacao.show', $captacao->id_captacao) }}" class="btn btn-sm btn-outline-info"><i class="fas fa-eye"></i></a>
-                                    </td>
-                                    
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                </table>
-                <div class="card-footer">
-                    @if (isset($filtros))
-                    {!! $captacoes->appends($filtros)->links()!!}
-                    @else
-                        {!! $captacoes->links()!!}    
-                    @endif
-                    
-                </div>
+            <table class="table table-responsive table-condensed table-hover">
+                
+                <thead>
+                    <th>#</th>
+                    <th>Ano Letivo</th>                        
+                    <th>Interessado</th>                        
+                    <th>Aluno</th>
+                    <th>Série</th>
+                    <th>Situação</th>                        
+                    <th width="170">Ações</th>
+                </thead>
+                <tbody>   
+                    @foreach ($captacoes as $index => $captacao)                       
+                        <tr>
+                            <td>{{$index+1}}</td>
+                            <td>
+                                {{$captacao->ano}}
+                            </td> 
+                            <td>
+                                {{$captacao->nome}}
+                            </td>                   
+                            <td>
+                                {{$captacao->aluno}}                                      
+                            </td>              
+                            <td>{{$captacao->serie_pretendida}}</td>
+                            <td>{{$captacao->tipo_negociacao}}</td>
+                            <td style="width=10px;">
+                                <a href="{{ route('historico.create', $captacao->id_captacao) }}" class="btn btn-sm btn-outline-primary"><i class="fas fa-paste"></i></a>
+                                <a href="{{ route('captacao.edit', $captacao->id_captacao) }}" class="btn btn-sm btn-outline-success"><i class="fas fa-edit"></i></a>
+                                <a href="{{ route('captacao.show', $captacao->id_captacao) }}" class="btn btn-sm btn-outline-info"><i class="fas fa-eye"></i></a>
+                            </td>
+                            
+                        </tr>
+                    @endforeach
+                </tbody>
+                
+            </table>
+            <div class="card-footer">
+                @if (isset($filtros))
+                {!! $captacoes->appends($filtros)->links()!!}
+                @else
+                    {!! $captacoes->links()!!}    
+                @endif
+                
+            </div>
         </div>
     </div>
 
