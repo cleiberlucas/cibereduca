6cc286e2bb991e9f01677c6efaf956d3135f5c33
Cleiber <cleiberlucas@gmail.com>
2020-12-10 11:54:47 -0300

    Update functions.php

--- app/Helpers/functions.php
+++ app/Helpers/functions.php
@@ -118,9 +118,10 @@ function nomeMes(int $mes)
         '10' => 'Outubro',
         '11' => 'Novembro',
         '12' => 'Dezembro',
+        '13' => 'Janeiro',
     );
 
-    return ($nomeMes[$mes]);
+    return ($nomeMes[$mes]); 
 }
 
 /**
