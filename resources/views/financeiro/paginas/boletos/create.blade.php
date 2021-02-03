a3f02e353c7b5ba7ce7141c97fd0197862e1cc45
Cleiber <cleiberlucas@gmail.com>
2021-02-03 10:31:55 -0300

    boleto

--- resources/views/financeiro/paginas/boletos/create.blade.php
+++ resources/views/financeiro/paginas/boletos/create.blade.php
@@ -52,7 +52,7 @@
                 </div>
                 <div class="col sm-4">
                     <h5>IMPORTANTE:</h5>
-                    <h6><strong>Vencimento:</strong> caso o vencimento original do recebível seja em final de semana ou feriado, o vencimento será recalculado para o próximo dia útil. 
+                    <h6><strong>Vencimento:</strong> caso o vencimento original do recebível seja em final de semana ou feriado, o vencimento do boleto será recalculado para o próximo dia útil. 
                         Este recálculo é necessário para permitir o pagamento com desconto.</h6>
                     <h6><strong>Registro do boleto no Banco:</strong> o boleto poderá ser pago, somente, após uma hora do envio da remessa ao Banco.</h6>
                 </div>
